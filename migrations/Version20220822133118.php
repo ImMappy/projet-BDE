3ff0143b0b5d2ab80b518ef5c2a5b8f9b38a0be3
paule44 <paulinerobial@gmail.com>
2022-08-22 16:00:11 +0200

    Liens connexion + deco sur buttons et label pseudo

---
+++ migrations/Version20220822133118.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220822133118 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE sortie (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(150) NOT NULL, date_heure_debut DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', duree INT NOT NULL, date_limite_inscription DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', nb_inscriptions_max INT NOT NULL, infos_sortie LONGTEXT DEFAULT NULL, etat VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE sortie');
+    }
+}
