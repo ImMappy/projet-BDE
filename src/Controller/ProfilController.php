9eaabe3325e3d4e1b680062dcbb19d5ea4f3ad1c
luis <luismariamontero@gmail.com>
2022-08-23 15:55:59 +0200

    Ajout id user

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\ModifProfilFormType;
+use App\Repository\CampusRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,15 +14,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilController extends AbstractController
 {
-    #[Route('/profil', name: 'app_profil')] /* get $id from l'URL, and don't forget {id} in ROUTE */
-    public function afficheProfil(): Response
+    #[Route('/profil/{id}', name: 'app_profil')] /* get $id from l'URL, and don't forget {id} in ROUTE */
+    public function afficheProfil(CampusRepository $campus): Response
     {
         return $this->render('profil/profil.html.twig', [
             'controller_name' => 'ProfilController',
+            'campus'=>$campus->find(1) // a modifier pour selection campus dynamique
         ]);
     }
 
-    #[Route('/profil/modif', name: 'app_modif_profil')] /* get $id from l'URL, and don't forget {id} in ROUTE */
+    #[Route('/profil/modif/{id}', name: 'app_modif_profil')] /* get $id from l'URL, and don't forget {id} in ROUTE */
     public function modifProfil(Request $request, EntityManagerInterface $em): Response
     {
         $user = new User();
