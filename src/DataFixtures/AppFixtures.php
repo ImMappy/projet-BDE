be174c16f660d0a30791c5dc516956398b7f979c
luis <luismariamontero@gmail.com>
2022-08-24 16:25:51 +0200

    Modification paths

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,9 +3,11 @@
 namespace App\DataFixtures;
 
 use App\Entity\Sortie;
+use App\Entity\Ville;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
+use Faker\Provider\fr_FR;
 
 class AppFixtures extends Fixture
 {
@@ -14,20 +16,14 @@ class AppFixtures extends Fixture
         public function load(ObjectManager $manager): void
         {
 
-//            $faker = Faker\Factory::create('fr_FR');
-//
-//            for($i = 0; $i < 30; $i++){
-//                $sortie[$i] = new Sortie();
-//                $sortie[$i]->setNom($faker->word);
-//                $sortie[$i]->setDateHeureDebut(new \DateTimeImmutable());
-//                $sortie[$i]->setDuree(mt_rand(0,20));
-//                $sortie[$i]->setDateLimiteInscription(new \DateTimeImmutable());
-//                $sortie[$i]->setNbInscriptionsMax(mt_rand(3,15));
-//                $sortie[$i]->setEtat("En cours");
-//                $sortie[$i]->setOrganisateur($sortie[$i]->getOrganisateur());
-//                $sortie[$i]->setInfosSortie($faker->text);
-//                $manager->persist($sortie[$i]);
-//            }
-//            $manager->flush();
+            $faker = Faker\Factory::create('fr_FR');
+
+            for($i = 0; $i < 5; $i++){
+               $ville[$i]= new Ville();
+               $ville[$i]->setNom($faker->city);
+               $ville[$i]->setCodePostal($faker->postcode);
+               $manager->persist($ville[$i]);
+            }
+            $manager->flush();
         }
 }
