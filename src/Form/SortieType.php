d4835d2f8c629b3046b590cc05300839e888fda3
luis <luismariamontero@gmail.com>
2022-08-24 17:11:46 +0200

    methode toString dans entity ville

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,8 +2,17 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
+use App\Entity\Etat;
+use App\Entity\Lieu;
 use App\Entity\Sortie;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,17 +21,43 @@ class SortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
-            ->add('nbInscriptionsMax')
-            ->add('infosSortie')
-            ->add('organisateur')
-            ->add('user')
-            ->add('campus')
-            ->add('etat')
-            ->add('lieu')
+            ->add('nom',TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control'
+                ]
+            ])
+            ->add('dateHeureDebut',DateType::class)
+            ->add('duree',NumberType::class,[
+                'attr'=> [
+                    'class'=>'form-control'
+                ],
+                'label'=>'Date début sortie'
+            ])
+            ->add('dateLimiteInscription',DateType::class)
+            ->add('nbInscriptionsMax',NumberType::class,[
+                'attr'=>[
+                    'class'=>'form-control'
+                ],
+                'label'=>"Date limite d'inscription"
+            ])
+            ->add('infosSortie',TextareaType::class,[
+                'attr'=>[
+                    'class'=>'form-control'
+                ],
+                'label'=>'Informations'
+            ])
+            ->add('campus',EntityType::class,[
+                'class'=>Campus::class,
+                'label'=>'Campus'
+            ])
+            ->add('etat',EntityType::class,[
+                'class'=>Etat::class,
+                'label'=>'Etat'
+            ])
+            ->add('lieu',EntityType::class,[
+                'class'=>Lieu::class,
+                'label'=>'Lieu'
+            ])
         ;
     }
 
