362115b4bfd8ef8fd0af09f4a425277528ad494b
luis <luismariamontero@gmail.com>
2022-08-25 11:47:38 +0200

    Formulaires personnalises + ajouts de filtres

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -40,32 +40,26 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::subMenu('Sorties', 'fa-solid fa-person-walking-arrow-right')->setSubItems([
             MenuItem::linkToCrud('Ajouter une sortie','fas fa-plus',Sortie::class)->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Modifier une sortie','fa-solid fa-pen',Sortie::class)->setAction(Crud::PAGE_EDIT),
             MenuItem::linkToCrud('Toutes les sorties','fa-solid fa-eye',Sortie::class)
         ]);
         yield MenuItem::subMenu('Campus', 'fa-solid fa-school')->setSubItems([
             MenuItem::linkToCrud('Ajouter un campus','fas fa-plus',Campus::class)->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Modifier un campus','fa-solid fa-pen',Campus::class)->setAction(Crud::PAGE_EDIT),
             MenuItem::linkToCrud('Tous les campus','fa-solid fa-eye',Campus::class)
         ]);
         yield MenuItem::subMenu('Étudiants','fa-solid fa-graduation-cap')->setSubItems([
             MenuItem::linkToCrud('Ajouter un étudiant','fas fa-plus',User::class)->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Modifier un profil','fas fa-pen',User::class)->setAction(Crud::PAGE_EDIT),
             MenuItem::linkToCrud('Tous les étudiants','fas fa-eye',User::class)
         ]);
         yield MenuItem::subMenu('État','fa-solid fa-spinner')->setSubItems([
             MenuItem::linkToCrud('Ajouter un état','fas fa-plus',Etat::class)->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Modifier un état','fas fa-pen',Etat::class)->setAction(Crud::PAGE_EDIT),
             MenuItem::linkToCrud('Tous les états','fas fa-eye',User::class)
         ]);
         yield MenuItem::subMenu('Lieux','fa-solid fa-location-pin')->setSubItems([
             MenuItem::linkToCrud('Ajouter un lieu','fas fa-plus',Lieu::class)->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Modifier un lieu','fas fa-pen',Lieu::class)->setAction(Crud::PAGE_EDIT),
             MenuItem::linkToCrud('Tous les lieux','fas fa-eye',Lieu::class)
         ]);
         yield MenuItem::subMenu('Villes','fa-solid fa-city')->setSubItems([
             MenuItem::linkToCrud('Ajouter une ville','fas fa-plus',Ville::class)->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Modifier une ville','fas fa-pen',Ville::class)->setAction(Crud::PAGE_EDIT),
             MenuItem::linkToCrud('Toutes les villes','fas fa-eye',Ville::class)
         ]);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
