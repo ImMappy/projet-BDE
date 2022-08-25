a046909ac1450546fc09dd4174073073c9793c6c
luis <luismariamontero@gmail.com>
2022-08-25 14:48:51 +0200

    ajout sensio bundle + user menu dans dashboard admin

--- src/Controller/Admin/SortieCrudController.php
+++ src/Controller/Admin/SortieCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Sortie;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
