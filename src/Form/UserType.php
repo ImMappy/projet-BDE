4630f1495133028db044186d74f380a846acc2d0
luis <luismariamontero@gmail.com>
2022-09-01 17:37:27 +0200

    retrait pour securite des roles

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -41,41 +41,29 @@ class UserType extends AbstractType
                     'class' => 'form-control'
                 ]
             ])
-            ->add('Roles', ChoiceType::class, [
-                'required' => true,
-                'multiple' => false,
-                'expanded' => false,
-                'choices' => [
-                    'User' => 'ROLE_USER',
-                    'Admin' => 'ROLE_ADMIN',
-                ],
-            ])
             ->add('image', FileType::class, [
                 'label' => false,
                 'multiple' => true,
                 'mapped' => false,
                 'required' => false
             ])
-
-            ->add('isAdministrateur')
-            ->add('isActif')
             ->add('submit',SubmitType::class,[
                 'attr'=>[
                     'class'=>'btn btn-outline-success my-4 text-center md-'
                 ],
                 'label'=>'Valider'
             ]);
-        $builder->get('Roles')
-            ->addModelTransformer(new CallbackTransformer(
-                function ($rolesArray) {
-                    // transform the array to a string
-                    return count($rolesArray)? $rolesArray[0]: null;
-                },
-                function ($rolesString) {
-                    // transform the string back to an array
-                    return [$rolesString];
-                }
-            ));
+    //        $builder->get('Roles')
+    //            ->addModelTransformer(new CallbackTransformer(
+    //                function ($rolesArray) {
+    //                    // transform the array to a string
+    //                    return count($rolesArray)? $rolesArray[0]: null;
+    //                },
+    //                function ($rolesString) {
+    //                    // transform the string back to an array
+    //                    return [$rolesString];
+    //                }
+    //            ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
